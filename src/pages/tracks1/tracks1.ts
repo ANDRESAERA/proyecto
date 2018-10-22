5ac3b594b171540d86130ac7d031c2a8e7bcdb14
Andres Eduardo Ramirez <andresoni_2000@hotmail.com>
2018-10-22 10:19:35 -0500

    informacion completa disco1

--- src/pages/tracks1/tracks1.ts
+++ src/pages/tracks1/tracks1.ts
@@ -15,6 +15,19 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class Tracks1Page {
 
+  tracks1 = [" One 4:13","I’m a Mess 4:05", "Sing 3:55","Don't 3:39","Nina 3:46", "Photograph 4:18",
+  "Bloodstream 5:00",
+  "Tenerife Sea 4:01",
+  "Runaway 3:25",
+  "The Man 4:10",
+  "Thinking Out Loud 4:41",
+  "Afire Love 5:14",
+  "Take It Back 3:28",
+  "Shirtsleeves 3:10",
+  "Even My Dad Does Sometimes 3:49",
+  "I See Fire 4:58",
+  "All of the Stars 3:55"]
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
